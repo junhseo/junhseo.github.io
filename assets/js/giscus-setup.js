4634dd01823e331dce3a3decb3b8fed10936db05
junhseo <48101303+junhseo@users.noreply.github.com>
2025-11-19 03:10:23 +0000

    Deploying to gh-pages from @ junhseo/junhseo.github.io@d9b784af165b6911c99fb96b030e87119c06dafb 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "junhseo/io",
+    "data-repo": "junhseo/junhseo.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
